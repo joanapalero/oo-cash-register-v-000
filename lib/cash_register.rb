dd708b09bbb40ebc09233b3f560d3929d1855cd5
Joana Palero <joana.palero24@gmail.com>
2018-07-02 02:59:34 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
+class CashRegister
+  attr_accessor :total, :transaction, :items, :employee, :discount
 
+  def initialize(employee = nil)
+    @total = 0
+    @items = []
+    @employee = employee
+    @discount = 20
+
+  end
+
+  
+end
